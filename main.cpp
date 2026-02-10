4cdb1102fe73a38efa1087d60ca6d8f7093f03dd
Carlos Leonardo Goes Farias <carlos.leonardo.goes@gmail.com>
2026-02-10 16:14:28 -0300

    Renomear variáveis de condição para maior clareza na lógica de produtor-consumidor

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include "Buffer.h"
 
 Buffer buffer;
-std::condition_variable cheio;
-std::condition_variable vazio;
+std::condition_variable nao_vazio;
+std::condition_variable nao_cheio;
 
 int main() {
     std::cout << "Exemplo de Produtor - Consumidor" << std::endl;
@@ -15,10 +15,10 @@ int main() {
         for (int i = 1; i <= buffer.MaximoValor; ++i) {
             std::unique_lock l{buffer.mutex};
             // Simula a produção de um item
-            vazio.wait(l, [&]() { return buffer.fila.size() < buffer.MaximoValor; });
+            nao_cheio.wait(l, [&]() { return buffer.fila.size() < buffer.MaximoValor; });
             buffer.fila.push(i);
             std::cout << "Produzido " << i << std::endl;
-            cheio.notify_one();
+            nao_vazio.notify_one();
             l.unlock();
             std::this_thread::sleep_for(std::chrono::seconds(1));
         }
@@ -28,11 +28,11 @@ int main() {
             std::unique_lock l{buffer.mutex};
 
             // Simula o consumo de um item
-            cheio.wait(l, [&]() { return !buffer.fila.empty(); });
-            int item = buffer.fila.front();
+            nao_vazio.wait(l, [&]() { return !buffer.fila.empty(); });
+            const int item = buffer.fila.front();
             buffer.fila.pop();
             std::cout << "Consumido " << item << std::endl;
-            vazio.notify_one();
+            nao_cheio.notify_one();
             l.unlock();
             std::this_thread::sleep_for(std::chrono::seconds(1));
         }
